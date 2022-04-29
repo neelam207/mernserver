e50ae6d817f637a206f082f0b350841bfde020dd
Bhardwaj <u1078807@inbgl1wlx14038.quintiles.net>
2022-04-29 15:33:27 +0530

    JSOn changes

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express();
 app.get('/home', (request, response) => {
-    response.send({data: 'Hi Testing'})
+    response.send({data: {
+        "message": "hello there",
+        code: 200
+    }})
 });
 const PORT = process.env.PORT || 5001
 app.listen(PORT)
