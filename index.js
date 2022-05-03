79aa05f540ea50c62c3f532b0518a99be00f5cef
Bhardwaj <u1078807@inbgl1wlx14038.quintiles.net>
2022-05-03 13:02:33 +0530

    Added List

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./service/passport')
 const authRountes = require('./routes/authRoutes')
 const app = express();
 authRountes(app)
-
+app.get('/list', (req, res) => res.send([1,2,3]))
 //client id 830849283125-cpdt17m05se7ak1cr0iefhr9c0jhh23v.apps.googleusercontent.com
 //client secret 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
 // app.get('/home', (request, response) => {
