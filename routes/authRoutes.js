f0435e530994835820a48d926dd516b100c20ec2
Bhardwaj <u1078807@inbgl1wlx14038.quintiles.net>
2022-05-06 13:07:04 +0530

    TEst1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 
 module.exports = (app) => {
-app.get('/list', (request, response) => {
+app.get('https://shielded-fjord-58470.herokuapp.com/list', (request, response) => {
     response.send({data: [1,2,3,4,5]})
 });
 // app.get('/auth/google', passport.authenticate("google", {
