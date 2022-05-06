85f1772ea2a26d414f2d183f799df205df35f0e5
Bhardwaj <u1078807@inbgl1wlx14038.quintiles.net>
2022-05-06 13:27:25 +0530

    TEst1

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+// const mongoose = require('mongoose');
+// const cookieSession = require('cookie-session');
+// const passport = require('passport');
+// const keys = require('./config/keys');
+// require('./models/User');
+// require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+// mongoose.connect(keys.mongoURI);
 
-const app = express();
+ const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//   cookieSession({
+//     maxAge: 30 * 24 * 60 * 60 * 1000,
+//     keys: [keys.keyCookie]
+//   })
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-
+// require('./routes/authRoutes')(app);
+app.get('/list', (request, response) => {
+    response.send({data: [1,2,3,4,5]})
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
