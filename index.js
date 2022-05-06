8a73153800d0754ab0089899b114f6291b3fd089
Bhardwaj <u1078807@inbgl1wlx14038.quintiles.net>
2022-05-06 13:25:07 +0530

    TEst1

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
-const express = require('express')
-const passport = require('passport')
-const cookies = require('cookie-session')
-require('./models/User')
-require('./service/passport')
-const authRountes = require('./routes/authRoutes')
-const keys = require('./config/keys')
+const express = require('express');
 const mongoose = require('mongoose');
-const cookieSession = require('cookie-session')
-mongoose.connect(keys.mongoURI)
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
-app.use(cookieSession({
-    maxAge: 30 *24 * 60 * 60 *1000,
-    keys: [keys.keyCookie]
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-authRountes(app)
 
-app.get('/list', (req, res) => res.send([1,2,3]))
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 
-// const PORT = process.env.PORT || 5001
-// app.listen(PORT)
-
-const server = app.listen(process.env.PORT || 5001, () => {
-    const port = server.address().port;
-    console.log(`Express is working on port ${port}`);
-  });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
