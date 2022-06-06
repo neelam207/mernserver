d8a2b83c320a782e29a43601a7aac64634c6c0ef
Rishabh Bhardwaj <rishabh.bhardwaj2@iqvia.com>
2022-06-06 11:33:06 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
 const express = require('express');
-// const mongoose = require('mongoose');
-// const cookieSession = require('cookie-session');
-// const passport = require('passport');
-// const keys = require('./config/keys');
-// require('./models/User');
-// require('./services/passport');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./service/passport');
 
-// mongoose.connect(keys.mongoURI);
-
+mongoose.connect(keys.mongoURI);
+const User = mongoose.model('users')
  const app = express();
+ var cors = require('cors');
+ app.use(cors());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.keyCookie]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-// app.use(
-//   cookieSession({
-//     maxAge: 30 * 24 * 60 * 60 * 1000,
-//     keys: [keys.keyCookie]
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// require('./routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 app.get('/list', (request, response) => {
     response.send({data: [1,2,3,4,5]})
 });
-const PORT = process.env.PORT || 5000;
+app.get("/users", (request, response) => {
+  
+  response.send({
+    data: {
+      length: User.length
+    }
+  })
+})
+const PORT = process.env.PORT || 5001;
 app.listen(PORT);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,15 +4,22 @@ module.exports = (app) => {
 app.get('https://shielded-fjord-58470.herokuapp.com/list', (request, response) => {
     response.send({data: [1,2,3,4,5]})
 });
-// app.get('/auth/google', passport.authenticate("google", {
-//     scope: ['profile', 'email']
-// }))
-// app.get('/auth/google/callback', passport.authenticate("google"))
-// app.get('/api/current_user', (request, response) => {
-//     response.send(request.user)
-// });
-// app.get('/api/logout', (request, response) => {
-//     request.logout()
-//     response.send(request.user)
-// });
+app.get('/auth/google', passport.authenticate("google", {
+    scope: ['profile', 'email']
+}))
+app.get('/auth/google/callback', passport.authenticate("google"))
+app.get('o', (request, response) => {
+    response.send(request.user)
+});
+app.get('/api/logout', (request, response) => {
+    request.logout()
+    response.send(request.user)
+});
+app.get('/api/current_user', (request, response) => {
+    const data = {
+        data: 1
+    }
+
+    response.send(request.user)
+});
 }
